78716c5700e88d46b040580e7e8f590c334e0ac6
Diego Banovaz <diego@strive.school>
2019-09-12 10:23:04 +0200

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const studentsRouter = require("./services/students")
 const projectsRouter = require("./services/projects")
 
 const server = express();
 
+server.use(cors())
 server.use(bodyParser.json())
 
 server.use("/students", studentsRouter);
